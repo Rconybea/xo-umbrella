b46048597137fd5da0b462273fa01a3d9b5cd07f
Roland Conybeare <rconybeare@gmail.com>
2024-03-29 14:48:18 -0400

    cosmetic: whitespace

--- src/optionutil/Size.cpp
+++ src/optionutil/Size.cpp
@@ -8,45 +8,45 @@
 #include "xo/reflect/Reflect.hpp"
 
 namespace xo {
-  using xo::json::PrintJson;
-  using xo::json::JsonPrinter;
-  using xo::reflect::Reflect;
-  using xo::reflect::TaggedPtr;
-  using xo::reflect::TypeDescr;
+    using xo::json::PrintJson;
+    using xo::json::JsonPrinter;
+    using xo::reflect::Reflect;
+    using xo::reflect::TaggedPtr;
+    using xo::reflect::TypeDescr;
 
-  namespace option {
-    namespace {
-      /* json printer for xo::option::Size */
-      class SizeJsonPrinter : public JsonPrinter {
-      public:
-    SizeJsonPrinter(PrintJson * pjson) : JsonPrinter(pjson) {}
+    namespace option {
+        namespace {
+            /* json printer for xo::option::Size */
+            class SizeJsonPrinter : public JsonPrinter {
+            public:
+                SizeJsonPrinter(PrintJson * pjson) : JsonPrinter(pjson) {}
 
-    virtual void print_json(TaggedPtr tp, std::ostream * p_os) const {
-      Size * x = tp.recover_native<Size>();
+                virtual void print_json(TaggedPtr tp, std::ostream * p_os) const {
+                    Size * x = tp.recover_native<Size>();
 
-      if (x) {
-        *p_os << x->to_int();
-      } else {
-        report_internal_type_consistency_error(Reflect::require<Size>(),
-                           tp.td(),
-                           p_os);
-      }
-    } /*print_json*/
-      }; /*SizeJsonPrinter*/
-    } /*namespace*/
+                    if (x) {
+                        *p_os << x->to_int();
+                    } else {
+                        report_internal_type_consistency_error(Reflect::require<Size>(),
+                                                               tp.td(),
+                                                               p_os);
+                    }
+                } /*print_json*/
+            }; /*SizeJsonPrinter*/
+        } /*namespace*/
 
-    void
-    Size::provide_json_printer(PrintJson * p_pjson)
-    {
-      assert(p_pjson);
+        void
+        Size::provide_json_printer(PrintJson * p_pjson)
+        {
+            assert(p_pjson);
 
-      TypeDescr td = Reflect::require<Size>();
+            TypeDescr td = Reflect::require<Size>();
 
-      std::unique_ptr<JsonPrinter> pr(new SizeJsonPrinter(p_pjson));
+            std::unique_ptr<JsonPrinter> pr(new SizeJsonPrinter(p_pjson));
 
-      p_pjson->provide_printer(td, std::move(pr));
-    } /*provide_json_printer*/
-  } /*namespace option*/
+            p_pjson->provide_printer(td, std::move(pr));
+        } /*provide_json_printer*/
+    } /*namespace option*/
 } /*namespace xo*/
 
 /* end Size.cpp */
