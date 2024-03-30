f50f39988ed19955c38a2e212fdb1987921151b2
Roland Conybeare <rconybeare@gmail.com>
2024-03-30 13:21:41 -0400

    emacs: + .dir-locals.el (commented out)

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;; see (info "(emacs) directory variables")
+
+;;((c++-mode . ((projectile--cmake-manual-command-alist . ((:configure-command "cmake -DCMAKE_INSTALL_PREFIX=~/scratch -DCMAKE_BUILD_TYPE=debug -B .build")
+;;                                                         (:compile-command "cmake --build .build")
+;;                                                         (:test-command . "cmake --build .build --target unit-tests"))))))
