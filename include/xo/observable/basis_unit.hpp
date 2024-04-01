8a040e5fc3e113eec37637437d0880bebf071ee4
Roland Conybeare <rconybeare@gmail.com>
2024-04-01 12:56:05 -0400

    xo-observable: cosmetic: variable rename

--- include/xo/observable/basis_unit.hpp
+++ include/xo/observable/basis_unit.hpp
@@ -9,13 +9,13 @@ namespace xo {
          *
          *  @brief A dimensionless multiple with natively-specified (i.e. at compile-time) dimension
          **/
-        template <dim DimId,
-                  native_unit_id NativeUnitId = native_unit_for_v<DimId>,
+        template <dim BasisDim,
+                  native_unit_id NativeUnitId = native_unit_for_v<BasisDim>,
                   typename InnerScale = std::ratio<1>>
         struct basis_unit {
             static_assert(ratio_concept<InnerScale>);
 
-            static constexpr dim c_native_dim = DimId;
+            static constexpr dim c_native_dim = BasisDim;
             static constexpr basis_unit c_native_unit = NativeUnitId;
 
             using inner_scalefactor_type = InnerScale;
@@ -53,32 +53,32 @@ namespace xo {
             static constexpr auto value = stringliteral("px");
         };
 
-        template<dim DimId>
-        constexpr auto native_unit_abbrev_v = native_unit_abbrev_helper<DimId>::value;
+        template<dim BasisDim>
+        constexpr auto native_unit_abbrev_v = native_unit_abbrev_helper<BasisDim>::value;
 
         // ----- scaled_native_unit_abbrev_helper -----
 
         namespace units {
             /* Require: InnerScale is ratio type; InnerScale >= 1 */
-            template <dim DimId, typename InnerScale>
+            template <dim BasisDim, typename InnerScale>
             struct scaled_native_unit_abbrev;
 
-            template <dim DimId>
-            struct scaled_native_unit_abbrev<DimId, std::ratio<1>> {
-                static constexpr auto value = native_unit_abbrev_v<DimId>;
+            template <dim BasisDim>
+            struct scaled_native_unit_abbrev<BasisDim, std::ratio<1>> {
+                static constexpr auto value = native_unit_abbrev_v<BasisDim>;
             };
 
-            template <dim DimId, typename InnerScale>
+            template <dim BasisDim, typename InnerScale>
             struct scaled_native_unit_abbrev {
                 /* e.g. unit of '10000 grams' will have abbrev '1000g' in absence
                  *      of a specialization for scaled_native_unit_abbrev
                  */
                 static constexpr auto value = stringliteral_concat(stringliteral_from_ratio<InnerScale>().value_,
-                                                                   native_unit_abbrev_helper<DimId>::value.value_);
+                                                                   native_unit_abbrev_helper<BasisDim>::value.value_);
             };
 
-            template <dim DimId, typename InnerScale>
-            constexpr auto scaled_native_unit_abbrev_v = scaled_native_unit_abbrev<DimId, InnerScale>::value;
+            template <dim BasisDim, typename InnerScale>
+            constexpr auto scaled_native_unit_abbrev_v = scaled_native_unit_abbrev<BasisDim, InnerScale>::value;
         }
     } /*namespace obs*/
 } /*namespace xo*/
